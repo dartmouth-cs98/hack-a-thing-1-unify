d740df8263b077f9bb0586d504a5b4ff1a20fa85
Gregory Hunter <gregoryhunter@Gregorys-MacBook-Pro.local>
2018-01-10 19:39:11 -0500

    Files from playing around with unity for some hours

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	
+	}
+	void FixedUpdate()
+	{
+		float moveHorizantal = Input.GetAxis ("Horizontal");
+		float MoveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizantal, 0.0f, MoveVertical);
+
+		rb.AddForce (movement * speed);
+
+
+	
+	}
+}
