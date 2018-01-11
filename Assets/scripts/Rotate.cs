b126e091c187df0a33292ee45565dd3fc1e9e1ff
Gregory Hunter <gregoryhunter@Gregorys-MacBook-Pro.local>
2018-01-10 20:18:30 -0500

    Getting the colors right for the test game

--- Assets/scripts/Rotate.cs
+++ Assets/scripts/Rotate.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class Rotate : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
 	}
 }
